00d9fb7eb972dd03da96a6e41bc73dc3515e3fc0
Cindy Ho <cindango@gmail.com>
2022-03-31 14:57:54 -0400

    fontsource for fonts

--- src/routes/__layout.svelte
+++ src/routes/__layout.svelte
@@ -1,6 +1,12 @@
 <script>
 	import Header from '$lib/header/Header.svelte';
 	import '../app.css';
+	import '@fontsource/barlow/300.css';
+	import '@fontsource/barlow/400.css';
+	import '@fontsource/barlow/500.css';
+	import '@fontsource/barlow-semi-condensed/300.css';
+	import '@fontsource/barlow-semi-condensed/400.css';
+	import '@fontsource/barlow-semi-condensed/500.css';
 	import logo from './../lib/header/InfimaLogo_Thin.svg';
 	import Time from "svelte-time";
 </script>
