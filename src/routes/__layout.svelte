a5f1d153b434b8e615525cf644e4424423b4754d
Cindy Ho <cindy@macbook-pro.myfiosgateway.com>
2022-04-13 15:53:27 -0400

    use setpath

--- src/routes/__layout.svelte
+++ src/routes/__layout.svelte
@@ -16,12 +16,11 @@
 	$: if($navigating) track();
 
 	function track() {
-		// Track a new page by updating the browser state:
-		// Update the browser state, showing "updated.html" in the browser address bar
-		var stateObj = { foo: 'updated' };
-		history.pushState(stateObj, "updated page", $navigating.to.pathname);
-		//Track the page view for the new page, '/updated.html'
+		// Track a new page using setPath:
+		// Update the path stored in the tracker:
 		var _hsq = window._hsq = window._hsq || [];
+		_hsq.push(['setPath', $navigating.to.pathname]);
+		// Track the page view for the new page
 		_hsq.push(['trackPageView']);
 	}
 </script>
