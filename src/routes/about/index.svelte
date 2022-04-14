1f259ff83e6433e00051766c5b96675782385972
Cindy Ho <cindy@macbook-pro.myfiosgateway.com>
2022-04-14 00:35:10 -0400

    pure css team popups

--- src/routes/about/index.svelte
+++ src/routes/about/index.svelte
@@ -11,7 +11,6 @@
 	import { onMount } from "svelte";
 	import * as prismicH from '@prismicio/helpers';
   import PrismicDom from 'prismic-dom';
-	import 'tw-elements';
 </script>
 
 <svelte:head>
@@ -46,44 +45,34 @@
 
           <div class="flex flex-row gap-4 person items-start">
             <div class="headshot flex-none">
-              <a data-bs-toggle="modal" data-bs-target="#{person.name[0].text.replace(/ /g,'_')}">
+              <label for="{person.name[0].text.replace(/ /g,'_')}" class="modal-button">
 								<img src="{person.headshot.url}" />
-							</a>
+							</label>
             </div>
             <div class="info">
-              <strong><a data-bs-toggle="modal" data-bs-target="#{person.name[0].text.replace(/ /g,'_')}">{person.name[0].text}</a></strong>
+              <strong><label for="{person.name[0].text.replace(/ /g,'_')}" class="modal-button">{person.name[0].text}</label></strong>
               <p>{person.title}</p>
             </div>
           </div>
 
-					<div class="modal fade fixed top-0 left-0 hidden w-full h-full outline-none overflow-x-hidden overflow-y-auto"
-					  id="{person.name[0].text.replace(/ /g,'_')}" aria-hidden="true">
-					  <div class="modal-dialog modal-xl modal-dialog-centered modal-dialog-scrollable relative w-auto pointer-events-none">
-					    <div
-					      class="modal-content border-none shadow-lg relative flex flex-col w-full pointer-events-auto bg-white bg-clip-padding rounded-md outline-none text-current">
-					      <div
-					        class="modal-header absolute w-full flex flex-shrink-0 items-center justify-between p-4 pb-0 rounded-t-md">
-					        <button type="button"
-					          class="btn-close box-content w-4 h-4 p-1 text-black border-none rounded-none opacity-50 focus:shadow-none focus:outline-none focus:opacity-100 hover:text-black hover:opacity-75 hover:no-underline"
-					          data-bs-dismiss="modal" aria-label="Close"></button>
-					      </div>
-					      <div class="modal-body relative p-4 lg:p-12">
-								<div class="flex flex-row gap-8 items-start">
-									<div class="headshot flex-none w-1/5">
-										<img src="{person.headshot.url}" />
-									</div>
-									<div class="profile w-4/5">
-										<h3>{person.name[0].text}</h3>
-										<span>{person.title}</span>
-										<div class="bio">
-											{@html prismicH.asHTML(person.bio)}
-										</div>
+					<input type="checkbox" id="{person.name[0].text.replace(/ /g,'_')}" class="modal-toggle">
+					<label for="{person.name[0].text.replace(/ /g,'_')}" class="modal cursor-pointer">
+					  <label class="modal-box w-11/12 max-w-5xl rounded-sm relative p-6 sm:p-10" for="">
+							<label for="{person.name[0].text.replace(/ /g,'_')}" class="close text-black bg-transparent border-0 absolute right-2 top-2"><a>✕</a></label>
+							<div class="flex flex-row gap-4 sm:gap-8 items-start">
+								<div class="headshot hidden sm:block flex-none w-1/5">
+									<img src="{person.headshot.url}" />
+								</div>
+								<div class="profile w-full sm:w-4/5">
+									<h3>{person.name[0].text}</h3>
+									<span>{person.title}</span>
+									<div class="bio w-full md:w-11/12">
+										{@html prismicH.asHTML(person.bio)}
 									</div>
 								</div>
-					      </div>
-					    </div>
-					  </div>
-					</div>
+							</div>
+					  </label>
+					</label>
 
           {/each}
 
@@ -142,12 +131,12 @@
     align-items: center;
   }
 
-	.person a {
-		text-decoration: none;
-		color: #fff;
+	.person label {
+		transition: color .5s ease;
+		cursor: pointer;
 	}
 
-	.person a:hover {
+	.person label:hover {
 		color: var(--primary-color-link);
 	}
 
@@ -157,6 +146,19 @@
     letter-spacing: .03rem;
     line-height: 1.2;
   }
+
+	.modal .close a {
+		text-decoration: none;
+		color: #000;
+		font-size: 2rem;
+		padding: 1rem;
+		opacity: .4;
+		transition: opacity .5s ease;
+	}
+
+	.modal .close a:hover {
+		opacity: 1;
+	}
 
 	.modal h3,
 	.modal .profile span {
